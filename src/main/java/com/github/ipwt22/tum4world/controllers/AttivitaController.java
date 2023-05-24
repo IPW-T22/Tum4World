95a7b9d368bb0523e0060bd3f1d86748bc4552ae
Goldo02 <andr3a.goldoni@gmail.com>
2023-05-24 07:21:55 +0200

    "risolto" problema sicurezza attività (la soluzione non piacerà a kevin)

--- src/main/java/com/github/ipwt22/tum4world/controllers/AttivitaController.java
+++ src/main/java/com/github/ipwt22/tum4world/controllers/AttivitaController.java
@@ -8,7 +8,8 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "attivita", value = "/attivita")
 public class AttivitaController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        if(request.getParameter("nomeAttivita")!=null)
+        String nomeAttivita = request.getParameter("nomeAttivita");
+        if(request.getParameter("nomeAttivita")!=null && !nomeAttivita.contains("/"))
             request.getRequestDispatcher("/WEB-INF/jsp/pubblico/attivita/" + request.getParameter("nomeAttivita")).forward(request, response);
         else
             request.getRequestDispatcher("/WEB-INF/jsp/pubblico/paginaattivita.jsp").forward(request, response);
