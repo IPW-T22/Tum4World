08ca6ebeb387bd51857e549095d399cbccd8d47e
Goldo02 <andr3a.goldoni@gmail.com>
2023-06-08 13:11:13 +0200

    sistemate iscrizioni per utente con token, bisogna cambiare l'url in modo che però sia dinamico a seconda di come è settato l'url base da chi esegue il codice, per il momento c'è hardcodato '/tum4world/'

--- src/main/webapp/js/changesub.js
+++ src/main/webapp/js/changesub.js
@@ -2,10 +2,18 @@
 //ogni volta che cambia lo stato di una delle checkbox, invia una richiesta ajax a "/tum4world/iscrizione" di tipo POST con i parametri "attivita_id" (codice dell'attivita) e "iscrizione" (true o false)
 //se la richiesta va a buon fine, aggiorna la pagina
 //se la risposta è un errore, annulla il cambiamento di stato della checkbox e informa l'utente
+
 function modificaIscrizione(id) {
     var attivita_id = "attivita_" + id;
     var iscrizione = document.getElementById(attivita_id).checked;
-    console.log(attivita_id + " " + iscrizione);
+    var urlOfRequest = "/tum4world/iscrizione";
+    var urlParams = new URLSearchParams(window.location.search);
+    var token = urlParams.get("token");
+
+    if(token!=null){
+        urlOfRequest += "?token=" + token;
+    }
+    console.log(attivita_id + " " + iscrizione + " " + token + " " + urlOfRequest);
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
@@ -18,7 +26,7 @@ function modificaIscrizione(id) {
             }
         }
     }
-    xhr.open("POST", "/tum4world/iscrizione", true);
+    xhr.open("POST", urlOfRequest, true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
     var data = "attivita_id=" + id + "&iscrizione=" + iscrizione;
